6b2b39463ca4e09122be71e3851a9bda9edcd2eb
vcutrona <v.cutrona1@gmail.com>
2016-01-16 18:39:44 +0100

    Form view skeleton

--- src/main/java/irproject/MainController.java
+++ src/main/java/irproject/MainController.java
@@ -5,10 +5,13 @@ import java.util.HashSet;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import entity.Greeting;
 import entity.UserFields;
 import extractor.UserClassification;
 import twitter.TweetExtractor;
@@ -34,9 +37,22 @@ public class MainController {
 	    }
     }
     
+    /*
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
+    }
+    */
+    @RequestMapping(value="/greeting", method=RequestMethod.GET)
+    public String greetingForm(Model model) {
+        model.addAttribute("greeting", new Greeting());
+        return "greeting";
+    }
+
+    @RequestMapping(value="/greeting", method=RequestMethod.POST)
+    public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+        model.addAttribute("greeting", greeting);
+        return "result";
     }
 }
